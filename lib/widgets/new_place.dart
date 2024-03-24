a8d60f3a969418b543c1d0949790730466235d59
Risal Walangadi <risal.walangadi@gmail.com>
2024-03-24 11:18:19 +0800

    add new place widget

---
+++ lib/widgets/new_place.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+import 'package:favorite_places/models/place.dart';
+
+class NewPlace extends StatefulWidget {
+  const NewPlace({super.key});
+
+  @override
+  State<NewPlace> createState() => _NewPlaceState();
+}
+
+class _NewPlaceState extends State<NewPlace> {
+  var _enteredName = "";
+
+  final _formKey = GlobalKey<FormState>();
+
+  void _addPlace() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      Navigator.of(context).pop<Place>(
+        Place(id: DateTime.now().toString(), name: _enteredName),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Tambah alamat baru")),
+      body: Form(
+        key: _formKey,
+        child: Container(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(
+                  icon: Icon(Icons.place),
+                  label: Text("Nama"),
+                ),
+                style: const TextStyle(color: Colors.white),
+                initialValue: _enteredName,
+                validator: (value) {
+                  if (value == null || value.trim().isEmpty) {
+                    return "Nama alamat tidak boleh kosong";
+                  }
+                  return null;
+                },
+                onSaved: (newValue) {
+                  _enteredName = newValue!;
+                },
+              ),
+              const SizedBox(height: 16),
+              ElevatedButton.icon(
+                onPressed: _addPlace,
+                label: const Text("Tambah Alamat"),
+                icon: const Icon(Icons.add),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/widgets/place.dart
+++ lib/widgets/place.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'package:favorite_places/models/place.dart';
+import 'package:favorite_places/widgets/new_place.dart';
+
 class PlaceWidget extends StatefulWidget {
   const PlaceWidget({super.key});
 
@@ -8,6 +11,19 @@ class PlaceWidget extends StatefulWidget {
 }
 
 class _PlaceWidgetState extends State<PlaceWidget> {
+  final List<Place> _listOfPlaces = [];
+
+  void _addPlace() async {
+    final result = await Navigator.of(context).push<Place>(
+      MaterialPageRoute(builder: (context) => const NewPlace()),
+    );
+    if (result != null) {
+      setState(() {
+        _listOfPlaces.add(result);
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,10 +31,16 @@ class _PlaceWidgetState extends State<PlaceWidget> {
         title: const Text("Alamat Anda"),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: _addPlace,
             icon: const Icon(Icons.add),
           ),
         ],
+      ),
+      body: ListView.builder(
+        itemCount: _listOfPlaces.length,
+        itemBuilder: ((context, index) {
+          return Text(_listOfPlaces[index].name);
+        }),
       ),
     );
   }
